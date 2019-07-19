6ef8a30b382a821d8fe9c0a49d15965d9c18b0af
Daniel de Souza Neri <danielneris01@gmail.com>
2019-07-18 23:41:17 -0300

    eu to puto

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Text, View, Image, TouchableOpacity } from 'react-native'
+import api from '../../services/api';
+
+import { Text, View, Image, TouchableOpacity, FlatList, StyleSheet } from 'react-native'
 
 import camera from '../../assets/camera.png';
 
@@ -12,9 +14,52 @@ export default class Feed extends Component {
         ),
     });
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            feed: [],
+        };
+    }
+
+    componentDidMount() {
+        //this.resgisterToSocket();
+        this.loadFeed();
+    }
+
+    loadFeed = async () => {
+        try {
+            const response = await api.get('posts');
+            console.log(response);
+            this.setState({ feed: response.data });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     render() {
+        const { feed } = this.state;
         return (
-            <View></View>
-        )
+            <View style={style.container}>
+                <FlatList
+                    data={feed}
+                    keyExtractor={post => post._id}
+                    renderItem={({ item }) => (
+                        <View style={style.feedItem}>
+                            <View style={style.feedItemHeader}>
+                                <View style={style.userInfo}>
+                                    <Text style={style.userInfo}></Text>
+                                </View>
+                            </View>
+                        </View>
+                    )}
+                />
+            </View>
+        );
     }
 }
+
+const style = StyleSheet.create({
+
+});
